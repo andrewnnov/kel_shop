a1d8a37125e38819caa117245139976d68557e7e
andrewnnov <andrewnnov@yandex.ru>
2023-09-28 19:48:09 -0300

    action

---
+++ src/test/java/com/kelshop/pages/BookPage.java
@@ -0,0 +1,10 @@
+package com.kelshop.pages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class BookPage {
+    private SelenideElement nameOfBook = $("div h1");
+
+}
